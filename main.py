0548e5afeadbc0098b2b7e63b093e0e4b570549b
Yuanming Hu <yuanmhu@gmail.com>
2018-04-15 13:29:04 -0400

    Simulation base

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import tensorflow as tf
 import cv2
 import os
+import numpy as np
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 batch_size = 1
 particle_count = 100
 gravity = (0, -9.8)
+dt = 1e-2
+total_steps = 10
 
 
 def polar_decomposition(m):
   assert False
 
-class States:
+# Initial State
+class InitialState:
   def __init__(self):
     self.position = tf.placeholder(tf.float32, [batch_size, particle_count, 2], name='position')
     self.velocity = tf.placeholder(tf.float32, [batch_size, particle_count, 2], name='velocity')
@@ -23,7 +27,25 @@ class States:
     mass, volume, Lame parameters (Young's modulus and Poisson's ratio)
     '''
 
-  
+# Updated state
+class UpdatedState:
+  def __init__(self, previous_state):
+    # Rotational velocity field
+    self.velocity = previous_state.position * np.array((-1, 1))[None, None, :]
+    # Advection
+    self.position = previous_state.position + self.velocity * dt
+
+class Simulation:
+  def __init__(self):
+    self.initial_state = InitialState()
+    self.updated_states = []
+    previous_state = self.initial_state
+
+    for i in range(total_steps):
+      new_state = UpdatedState(previous_state)
+      self.updated_states.append(new_state)
+      previous_state = new_state
+
 
 
 def main():
@@ -35,6 +57,9 @@ def main():
 
   x = tf.placeholder(tf.float32, [batch_size, 1], name='x')
   y0 = tf.placeholder(tf.float32, [batch_size, 1], name='y0')
+
+  sim = Simulation()
+
 
 
 if __name__ == '__main__':
