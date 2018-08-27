5c0aa5432a65aafe9427b40bedaafa06bc3c8d5e
Yuanming Hu <yuanmhu@gmail.com>
2018-08-26 22:12:51 -0400

    3d infrastructure

---
+++ src/simulator.cpp
@@ -0,0 +1,31 @@
+#include <taichi/common/util.h>
+
+TC_NAMESPACE_BEGIN
+
+class DMPMSimulator3D {
+
+  public:
+
+  DMPMSimulator3D() {
+
+  }
+
+  struct State {
+
+  };
+
+  void forward(const State &state) {
+
+  }
+
+  void backward(const State &initial_state, const State &new_state) {
+
+  }
+
+  void test() {
+
+  }
+
+};
+
+TC_NAMESPACE_END
