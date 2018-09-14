Fixed scaling of problem

demos/finger/finger_sim.py
@@ -19,7 +19,7 @@ gamma = 0.0
 
 sample_density = 20
 group_num_particles = sample_density**2
-goal_pos = np.array([1.4, 0.4])
+goal_pos = np.array([0.7, 0.4])
 goal_range = np.array([0.0, 0.00])
 batch_size = 1
 actuation_strength = 4
@@ -107,7 +107,7 @@ def generate_sim():
     return total_actuation, debug
   
   
-  res = (80, 40)
+  res = (40, 40)
   bc = get_bounding_box_bc(res)
   bc[0][:, :, :5] = -1 # Sticky
   bc[1][:, :, :5] = 0 # Sticky